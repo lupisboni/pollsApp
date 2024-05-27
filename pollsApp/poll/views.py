8cba1d3b510f17c4946d76bc5971a9ece8e68d08
lupisboni <guadaboni03@gmail.com>
2024-05-27 13:49:49 -0600

    commit 3

--- pollsApp/poll/views.py
+++ pollsApp/poll/views.py
@@ -1,21 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from .models import Quiestion
+
 # Create your views here.
 def home(request):
     questions = Quiestion.objects.all()
-    return render(
-        request,
+    return render( 
+        request, 
         'poll/home.html',
         {
             "questions": questions
-        }
-    )
+         })
 
 def vote(request, q_id):
-    q=Quiestion.objects.get(pk=q_id)
-    return render(request, 'poll/vote.html', {
+    q= get_object_or_404(Quiestion, pk=q_id)
+    if request.method == "POST":
+        choice_id = request.POST.get('choise')
+        print(choice_id)
+        choice = q.choice_set.get(pk=choice_id)
+        choice.votes += 1
+        choice.save()
+        return redirect('poll:result', q_id)
+    return render(
+        request, 
+        'poll/vote.html', 
+        {
+            "question": q
+         })
 
-    })
 def result(request, q_id):
-    pass
+    try:
+        q = Quiestion.objects.get(pk=q_id)
+    except Quiestion.DoesNotExist:
+        return redirect('poll:home')
+    return render(request, 'poll/result.html', {
+        "question": q
+    })
